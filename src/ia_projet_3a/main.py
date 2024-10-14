e9fd90c1ecb4072c55225e3e0c81e4c597d2a964
Marc <mmoreeuw@ensc.fr>
2024-10-14 23:01:42 +0200

    fix server code

--- src/ia_projet_3a/main.py
+++ src/ia_projet_3a/main.py
@@ -1,7 +1,8 @@
 import subprocess
 import time
-from enum import Enum
+import struct
 import socket
+from enum import Enum
 
 class Action(Enum):
     MOVE_RIGHT = 'Right'
@@ -14,16 +15,17 @@ class Action(Enum):
 
 def perform_action(actions, duration):
     """
-    Args :
-    actions : List of actions from Action enum (ex : [Action.RUN, Action.MOVE_RIGHT])
-    duration : duration of the action (ex: 3 for 3sec)
+    Args:
+    actions: List of actions from Action enum (e.g., [Action.RUN, Action.MOVE_RIGHT])
+    duration: duration of the action (e.g., 3 for 3 seconds)
     """
-
+    # Key down for each action
     for action in actions:
         subprocess.Popen(['xdotool', 'keydown', action.value])
 
     time.sleep(duration)
 
+    # Key up for each action
     for action in actions:
         subprocess.run(['xdotool', 'keyup', action.value])
 
@@ -45,19 +47,27 @@ print("Waiting for client")
 client = connect_to_server()
 print("Client launched")
 
-
 print("====================")
 
 print("Client listening...")
 
 while True:
-    data = client.recv(1024)
-    if not data:
+    data = bytearray()  # Initialize a bytearray to accumulate data
+    while len(data) < 2:  # Ensure we receive exactly 2 bytes
+        packet = client.recv(2 - len(data))  # Receive the remaining bytes needed
+        if not packet:
+            break
+        data.extend(packet)
+
+    if len(data) != 2:
+        print(f"Unexpected data length: {len(data)}")
         break
-    X_pos = data.decode('utf-8')
 
-    if X_pos == "90":
-        perform_action([Action.JUMP],1)
+    x_position, y_position = struct.unpack('BB', data)
+    print(f"Received Position - X: {x_position}, Y: {y_position}")
 
+    # Check the X position to perform an action
+    if x_position == 90:  # Adjust this condition as needed
+        perform_action([Action.JUMP], 1)
 client.close()
 print("Client died")
