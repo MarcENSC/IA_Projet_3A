0ea88b9c177951de73e3798627a2375b20b2ac8b
Clotaire MARDON <cmardon001@ensc.fr>
2024-10-15 14:03:06 +0200

    back to 48d0f7c9 with upgrades

--- src/ia_projet_3a/main.py
+++ src/ia_projet_3a/main.py
@@ -3,6 +3,7 @@ import time
 from enum import Enum
 import socket
 
+
 class Action(Enum):
     MOVE_RIGHT = 'Right'
     MOVE_LEFT = 'Left'
@@ -12,13 +13,13 @@ class Action(Enum):
     ENTER = 'Y'
     ESCAPE = 'T'
 
+
 def perform_action(actions, duration):
     """
     Args :
     actions : List of actions from Action enum (ex : [Action.RUN, Action.MOVE_RIGHT])
     duration : duration of the action (ex: 3 for 3sec)
     """
-
     for action in actions:
         subprocess.Popen(['xdotool', 'keydown', action.value])
 
@@ -27,10 +28,14 @@ def perform_action(actions, duration):
     for action in actions:
         subprocess.run(['xdotool', 'keyup', action.value])
 
+def press(action):
+    subprocess.Popen(['xdotool', 'keydown', action.value])
+
 def connect_to_server():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 8080))
     return client_socket
+
 
 ################### SCRIPT ###################
 
@@ -45,19 +50,38 @@ print("Waiting for client")
 client = connect_to_server()
 print("Client launched")
 
+print("====================")
+
+time.sleep(1)
+
+print("Starting game...")
+perform_action([Action.ENTER],1)
+time.sleep(3)
 
 print("====================")
 
 print("Client listening...")
 
+# press(Action.MOVE_RIGHT)
+
 while True:
+    # Receive data
     data = client.recv(1024)
+
     if not data:
         break
-    X_pos = data.decode('utf-8')
 
-    if X_pos == "90":
-        perform_action([Action.JUMP],1)
+    # read out data
+    decoded_data = data.decode('utf-8').strip()
+
+    # split data to get pos_x and pos_y
+    positions = decoded_data.split(',')
+
+    if len(positions) >= 2:
+        x_pos, y_pos = map(int, positions[:2])
+        print(x_pos,y_pos)
+        # if abs(enemy_x_pos - x_pos) < 10:
+        #     press(Action.JUMP)
 
 client.close()
 print("Client died")
